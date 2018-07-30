599e6c1a2a72dd096c0a4424fb9fae387760c107
xsh <2949055816@qq.com>
2018-07-30 11:31:15 +0800

    modify TxtRead.java

--- src/code/TxtRead.java
+++ src/code/TxtRead.java
@@ -1,19 +1,40 @@
 package code;
+
 import java.io.*;
+import java.util.*;
 public class TxtRead {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		// TODO 自动生成的方法存根
-		try{
-		File scoreTable=new File("data/scoreTable.txt");
-		File student=new File("data/student.txt");
-		BufferedReader brstu=new BufferedReader(new FileReader(scoreTable));
-		BufferedWriter bwst=new BufferedWriter(new FileWriter(student));
-		String s=null;
-		
-		}catch (Exception e) {
-			// TODO: handle exception
-			e.printStackTrace();
+		BufferedWriter bwst = null;
+		BufferedReader brstu = null;
+		try {
+			File scoreTable = new File("data/scoreTable.txt");
+			File student = new File("data/student.txt");
+			bwst = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(scoreTable,true)));
+			brstu = new BufferedReader(new InputStreamReader(new FileInputStream(student)));
+			String s = null;
+			Random ywRandom=new Random();
+			Random yyRandom=new Random();
+			Random sxRandom=new Random();
+			
+			while ((s = brstu.readLine()) != null) {
+				s+="\t";
+				s+=ywRandom.nextInt(51)+50;
+				s+="\t";
+				s+=yyRandom.nextInt(51)+50;
+				s+="\t";
+				s+=sxRandom.nextInt(51)+50;
+				s += System.lineSeparator();
+				
+				bwst.write(s);
+			}
+		} finally {
+			if (bwst != null)
+				bwst.close();
+			if (brstu != null)
+				brstu.close();
+
 		}
 	}
 
